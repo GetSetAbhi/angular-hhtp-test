a9930429412c177abc648f591029925e6538ac29
GetSetAbhi <araina97@gmail.com>
2021-05-26 21:58:48 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-troff7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-troff7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, VERSION } from '@angular/core';
+/***
+ * Fake API created at
+ *
+ * **/
+interface User {
+  name: string;
+  city: string;
+}
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  name = 'Angular ' + VERSION.major;
+  url = 'https://mocki.io/v1/d9df7a23-63e6-4bb0-861a-ff9a1cb2172c';
+  user: User;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<User>(this.url).subscribe(data => {
+      console.log(data);
+      this.user = data;
+    });
+  }
+}
